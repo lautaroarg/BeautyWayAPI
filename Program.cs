502a0dd3782c4ba5070c2a7ec2237f7adf3d4531
lautaro <lautarosoto007@hotmail.com>
2022-04-06 17:54:33 -0300

    Connection Configured

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
+using BeautyWayAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
+builder.Services.AddDbContext<BeautyWayContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
